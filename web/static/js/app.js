3cea04986af833044b70e8bdf4b482053b6749f2
Travis Vander Hoop <vanderhoop@me.com>
2017-03-06 09:27:13 -0600

    Enable react hot module replacement

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,6 +4,7 @@ import React from "react"
 import { render } from "react-dom"
 import { createStore } from "redux"
 import { Provider } from "react-redux"
+import { AppContainer } from "react-hot-loader"
 
 import RemoteRetro from "./components/remote_retro"
 import RetroChannel from "./services/retro_channel"
@@ -13,12 +14,23 @@ const userToken = window.userToken
 
 const retroChannelConfiguration = { userToken, retroUUID: window.retroUUID }
 const retroChannel = RetroChannel.configure(retroChannelConfiguration)
-
 const reactRoot = document.querySelector(".react-root")
 
-render(
-  <Provider store={createStore(rootReducer)}>
-    <RemoteRetro retroChannel={retroChannel} userToken={userToken} />
-  </Provider>,
-  reactRoot
-)
+const renderWithHotReload = RemoteRetro => {
+  render(
+    <AppContainer>
+      <Provider store={createStore(rootReducer)}>
+        <RemoteRetro retroChannel={retroChannel} userToken={userToken} />
+      </Provider>
+    </AppContainer>,
+    reactRoot
+  )
+}
+
+renderWithHotReload(RemoteRetro)
+
+if (module.hot) {
+  module.hot.accept("./components/remote_retro", () => {
+    renderWithHotReload(RemoteRetro)
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,30 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 const WebpackNotifierPlugin = require("webpack-notifier")
 
+const DEV_SERVER_PORT = 5001
+const OUTPUT_PATH = `${__dirname}/priv/static`
+const OUTPUT_PUBLIC_PATH = `http://localhost:${DEV_SERVER_PORT}/`
+
 module.exports = {
   entry: [
     "./web/static/js/polyfills/array.find",
     "./web/static/js/polyfills/array.find_index",
+    "react-hot-loader/patch",
+    `webpack-dev-server/client?${OUTPUT_PUBLIC_PATH}`,
+    "webpack/hot/only-dev-server",
     "./web/static/css/app.css",
     "./web/static/js/app.js"
   ],
   output: {
-    path: "./priv/static",
-    filename: "js/app.js"
+    path: OUTPUT_PATH,
+    filename: "js/app.js",
+    publicPath: OUTPUT_PUBLIC_PATH,
+  },
+  devServer: {
+    hot: true,
+    port: DEV_SERVER_PORT,
+    contentBase: OUTPUT_PATH,
+    publicPath: OUTPUT_PUBLIC_PATH,
   },
   resolve: {
     modules: ["node_modules", __dirname + "/web/static/js"],
@@ -56,7 +70,9 @@ module.exports = {
   },
   devtool: "cheap-module-eval-source-map",
   plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
     new WebpackNotifierPlugin({ skipFirstNotification: true }),
+    new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin({ filename: "css/app.css" }),
     new CopyWebpackPlugin([{ from: "./web/static/assets" }]),
     new webpack.DefinePlugin({
